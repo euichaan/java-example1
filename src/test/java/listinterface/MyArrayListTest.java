7ea34eb93f40c7f714164ae50d1ae6aaf908cc29
euichanhwang <pine6971@naver.com>
2023-03-11 16:34:32 +0900

    myArrayList 구현

---
+++ src/test/java/listinterface/MyArrayListTest.java
@@ -0,0 +1,32 @@
+package listinterface;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyArrayListTest {
+
+	private List<Integer> mylist;
+	private List<Integer> list;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+
+		mylist = new MyArrayList<Integer>();
+		mylist.addAll(list);
+	}
+
+	@Test
+	void testMyList() {
+		assertThat(mylist.size()).isEqualTo(3);
+	}
+
+}
