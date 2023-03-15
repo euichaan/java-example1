8654dcf0fdf1413ae6ce3d2ac33e4110fd735373
euichanhwang <pine6971@naver.com>
2023-03-15 11:13:47 +0900

    feat : LinkedList를 구현한다

---
+++ src/main/java/listinterface/ListNode.java
@@ -0,0 +1,27 @@
+package listinterface;
+
+public class ListNode {
+
+	public Object data;
+	public ListNode next;
+
+	public ListNode() {
+		this.data = null;
+		this.next = null;
+	}
+
+	public ListNode(Object data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public ListNode(Object data, ListNode next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "ListNode(" + data.toString() +")";
+	}
+}
---
+++ src/test/java/listinterface/MyLinkedListTest.java
@@ -0,0 +1,5 @@
+package listinterface;
+
+class MyLinkedListTest {
+
+}
